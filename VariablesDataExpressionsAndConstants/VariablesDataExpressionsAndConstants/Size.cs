4e9dc74494ee375bebb0779c17bac68e9df7f7b1
Dimitar Mitev <dnmitev@gmail.com>
2014-05-21 12:26:46 +0300

    Variables, Data, Expressions and Constants

---
+++ VariablesDataExpressionsAndConstants/VariablesDataExpressionsAndConstants/Size.cs
@@ -0,0 +1,79 @@
+﻿namespace VariablesDataExpressionsAndConstants
+{
+    using System;
+
+    public class Size
+    {
+        private double width;
+        private double height;
+
+        public Size(double givenWidth, double givenHeight)
+        {
+            this.Width = givenWidth;
+            this.Height = givenHeight;
+        }
+
+        public double Width
+        {
+            get
+            {
+                return this.width;
+            }
+
+            set
+            {
+                if (value > 0)
+                {
+                    this.width = value;
+                }
+                else
+                {
+                    throw new IndexOutOfRangeException("Width must be positive number");
+                }
+            }
+        }
+
+        public double Height
+        {
+            get
+            {
+                return this.height;
+            }
+
+            set
+            {
+                if (value > 0)
+                {
+                    this.height = value;
+                }
+                else
+                {
+                    throw new IndexOutOfRangeException("Width must be positive number");
+                }
+            }
+        }
+
+        public static Size GetRotatedSize(Size currentSize, double rotationAngle)
+        {
+            double newWidth = GetRotatedDimension(rotationAngle, currentSize.Width);
+            double newHeight = GetRotatedDimension(rotationAngle, currentSize.Height);
+
+            Size result = new Size(newWidth, newHeight);
+            
+            return result;
+        }
+
+        // I've added this method because I observed that in the given code the calcs about
+        // new dimensions are repeated and in that manner I am into the DRY concept
+        private static double GetRotatedDimension(double rotationAngle, double dimension)
+        {
+            double cosinus = Math.Cos(rotationAngle);
+            double sinus = Math.Sin(rotationAngle);
+            double result = Math.Abs(sinus * dimension) + Math.Abs(cosinus * dimension);
+
+            return result;
+        }
+
+    }
+
+}
---
+++ VariablesDataExpressionsAndConstants/VariablesDataExpressionsAndConstants/Statistic.cs
@@ -0,0 +1,67 @@
+﻿namespace VariablesDataExpressionsAndConstants
+{
+    using System;
+
+    public class Statistic
+    {
+        public void PrintStatistics(double[] numerics, int count)
+        {
+            double max = GetMaxElement(numerics, count);
+            double min = GetMinElement(numerics, count);
+            double average = GetAverage(numerics, count);
+
+            Print(max);
+            Print(min);
+            Print(average);
+        }
+
+        private double GetMaxElement(double[] elements, int count)
+        {
+            double max = elements[0];
+
+            for (int i = 1; i < count; i++)
+            {
+                if (elements[i] > max)
+                {
+                    max = elements[i];
+                }
+            }
+
+            return max;
+        }
+
+        private double GetMinElement(double[] elements, int count)
+        {
+            double min = elements[0];
+
+            for (int i = 1; i < count; i++)
+            {
+                if (elements[i] < min)
+                {
+                    min = elements[i];
+                }
+            }
+
+            return min;
+        }
+
+        private double GetAverage(double[] elements, int count)
+        {
+            double sum = 0;
+
+            for (int i = 0; i < count; i++)
+            {
+                sum += elements[i];
+            }
+
+            double average = sum / count;
+
+            return average;
+        }
+
+        private void Print(double parameter)
+        {
+            Console.WriteLine(parameter);
+        }
+    }
+}
