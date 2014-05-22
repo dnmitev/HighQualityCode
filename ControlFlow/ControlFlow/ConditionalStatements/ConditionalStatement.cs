020cd6245463027f1c5438f3d9752ec793721f04
Dimitar Mitev <dnmitev@gmail.com>
2014-05-22 22:59:35 +0300

    control flow hw is done

--- ControlFlow/ControlFlow/ConditionalStatements/ConditionalStatement.cs
+++ ControlFlow/ControlFlow/ConditionalStatements/ConditionalStatement.cs
@@ -2,15 +2,49 @@
 {
     using ControlFlow.StraightlineCode;
     using System;
-    using System.Collections.Generic;
-    using System.Linq;
-    using System.Text;
-    using System.Threading.Tasks;
 
     public class ConditionalStatement
     {
-        Potato potato = new Potato();
+        static void Main()
+        {
+            Potato potato = new Potato();
 
+            if (potato != null && potato.IsHealthy && potato.IsPeeled)
+            {
+                Cook(potato);
+            }
 
+            int x = 0;
+            int xMin = 0;
+            int xMax = 0;
+
+            int y = 0;
+            int yMin = 0;
+            int yMax = 0;
+
+            bool shouldVisitCell = true;
+
+            bool isValidX = xMin <= x && x <= xMax;
+            bool isValidY = yMin <= y && y <= yMax;
+
+            bool isCellValid = shouldVisitCell;
+
+            if (isCellValid && isValidX && isValidY)
+            {
+                VisitCell();
+            }
+        }
+ 
+        private static void VisitCell()
+        {
+            // TODO: Implement this method
+            throw new NotImplementedException();
+        }
+ 
+        private static void Cook(Potato potato)
+        {
+            // TODO: Implement this method
+            throw new NotImplementedException();
+        }
     }
 }
---
+++ ControlFlow/ControlFlow/Loops/Loop.cs
@@ -0,0 +1,24 @@
+﻿namespace ControlFlow.Loops
+{
+    using System;
+
+    class Loop
+    {
+        static void Main()
+        {
+            int[] integersArray = new int[100];
+            int expectedValue = 0;
+
+            for (int i = 0; i < integersArray.Length; i++)
+            {
+                Console.WriteLine(integersArray[i]);
+
+                if (i % 10 == 0 && integersArray[i] == expectedValue)
+                {
+                    Console.WriteLine("Value found");
+                    break;
+                }
+            }
+        }
+    }
+}
