e91d3bdd69cbe29d80cf4a55a659e50dac79e2fa
Dimitar Mitev <dnmitev@gmail.com>
2014-08-05 12:56:50 +0300

    All files added

---
+++ Calendar-System-Problem/CalendarSystem.Tests/AddEventTests.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CalendarSystem.Command;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CalendarSystem.Tests
+{
+    [TestClass]
+    public class AddEventTests
+    {
+        private EventEntry @event;
+        private IEventsManager manager;
+
+        [TestInitialize]
+        public void GetInstantiated()
+        {
+            this.manager = new EventsManager();
+            this.@event = new EventEntry
+                            {
+                                Date = new DateTime(2014, 08, 06, 16, 00, 00),
+                                Title = "HQPC Practical Exam",
+                                Location = "Telerik Academy"
+                            };
+        }
+
+        [TestMethod]
+        public void AddValidEntryShouldWork()
+        {
+            this.manager.AddEvent(this.@event);
+
+            var result = this.manager.ListEvents(@event.Date, 1).ToList();
+
+            Assert.AreEqual(1, result.Count);
+        }
+    }
+}
---
+++ Calendar-System-Problem/CalendarSystem.Tests/CommandParserTests.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CalendarSystem.Parser;
+
+namespace CalendarSystem.Tests
+{
+    [TestClass]
+    public class CommandParserTests
+    {
+        private ICommandParser parser;
+
+        [TestInitialize]
+        public void GetParserInstance()
+        {
+            this.parser = new CommandParser();
+        }
+
+        [TestMethod]
+        public void PassValidAddCommandStringShouldReturnValidCommand()
+        {
+            string addCmd = "AddEvent 2012-01-21T20:00:00 | party Viki | home";
+
+            var addCmdResult = this.parser.Parse(addCmd);
+
+            Assert.AreEqual("AddEvent", addCmdResult.Name);
+            Assert.AreEqual(3, addCmdResult.Params.Count);
+        }
+
+        [TestMethod]
+        public void PassValidDeleteCommandStringShouldReturnValidCommand()
+        {
+            string delCmd = "DeleteEvents c# exam";
+
+            var delCmdResult = this.parser.Parse(delCmd);
+
+            Assert.AreEqual("DeleteEvents", delCmdResult.Name);
+            Assert.AreEqual(1, delCmdResult.Params.Count);
+
+        }
+
+        [TestMethod]
+        public void PassValidListCommandStringShouldReturnValidCommand()
+        {
+            string listCmd = "ListEvents 2012-03-07T08:00:00 | 2";
+
+            var listCmdResult = this.parser.Parse(listCmd);
+
+            Assert.AreEqual("ListEvents", listCmdResult.Name);
+            Assert.AreEqual(2, listCmdResult.Params.Count);
+        }
+
+        [TestMethod]
+        public void PassAStringAlwaysShouldReturnCommandName()
+        {
+            string cmd = "AddEventPesho 2012-01-21T20:00:00 | party Viki | home";
+
+            var result = this.parser.Parse(cmd);
+
+            Assert.AreEqual("AddEventPesho", result.Name);
+        }
+    }
+}
